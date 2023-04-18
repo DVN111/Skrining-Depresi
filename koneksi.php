1c1a1b4185d0e59226d3d46ed37a1fbe6f2bf072
MUHAMMAD DANIYAL <79429893+DVN111@users.noreply.github.com>
2023-04-18 22:47:03 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Menghubungkan ke database
-  $servername = "localhost";
-  $db_email = "root";
-  $db_password = "";
-  $dbname = "users";
-  
-  $conn = mysqli_connect($servername, $db_email, $db_password, $dbname);
-  ?>
