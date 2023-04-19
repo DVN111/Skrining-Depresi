d27b573cb6efd379ba9c04c6a1daeabd651a02f5
MUHAMMAD DANIYAL <79429893+DVN111@users.noreply.github.com>
2023-04-19 11:41:59 +0700

    Add files via upload

--- marvel/database/users.sql
+++ marvel/database/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Apr 2023 pada 11.56
+-- Waktu pembuatan: 19 Apr 2023 pada 06.41
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -32,23 +32,27 @@ CREATE TABLE `skrining` (
   `user_id` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `hasil` varchar(100) NOT NULL,
-  `saran` varchar(255) NOT NULL
+  `saran` varchar(255) NOT NULL,
+  `nilai` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `skrining`
 --
 
-INSERT INTO `skrining` (`id`, `user_id`, `tanggal`, `hasil`, `saran`) VALUES
-(1, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(2, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(3, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(4, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(5, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(6, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(7, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(8, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat'),
-(9, 1, '2023-04-13', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat');
+INSERT INTO `skrining` (`id`, `user_id`, `tanggal`, `hasil`, `saran`, `nilai`) VALUES
+(14, 1, '2023-04-18', 'Depresi Berat', 'Perlu bantuan profesional berupa konseling dan terapi obat, kemungkinan untuk rawat inap', 42),
+(15, 1, '2023-04-19', 'Depresi Berat', 'Perlu bantuan profesional berupa konseling dan terapi obat, kemungkinan untuk rawat inap', 63),
+(16, 1, '2023-04-20', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat', 21),
+(17, 1, '2023-04-19', 'Depresi Berat', 'Perlu bantuan profesional berupa konseling dan terapi obat, kemungkinan untuk rawat inap', 57),
+(18, 1, '2023-04-19', 'Depresi Berat', 'Perlu bantuan profesional berupa konseling dan terapi obat, kemungkinan untuk rawat inap', 57),
+(19, 1, '2023-04-19', 'Depresi Berat', 'Perlu bantuan profesional berupa konseling dan terapi obat, kemungkinan untuk rawat inap', 57),
+(20, 1, '2023-04-21', 'Normal', 'Tidak perlu intervensi', 0),
+(21, 1, '2023-04-22', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat', 21),
+(22, 1, '2023-04-19', 'Depresi Ringan', 'Perlu dipelajari beberapa teknik relaksasi, pernafasan, olahraga terukur, dan diet', 12),
+(23, 1, '2023-04-23', 'Depresi Ringan', 'Perlu dipelajari beberapa teknik relaksasi, pernafasan, olahraga terukur, dan diet', 13),
+(24, 1, '2023-04-23', 'Depresi Ringan', 'Perlu dipelajari beberapa teknik relaksasi, pernafasan, olahraga terukur, dan diet', 13),
+(25, 1, '2023-04-24', 'Depresi Sedang', 'Perlu bantuan profesional berupa konseling dan terapi obat', 20);
 
 -- --------------------------------------------------------
 
@@ -68,7 +72,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `email`, `name`, `password`) VALUES
-(1, 'user@gmail.com', 'User Baru', '1');
+(1, 'user@gmail.com', 'User Baru', '1'),
+(2, 'user2@gmail.com', 'Muhammad Daniyal', '1'),
+(3, 'user2@gmail.com', 'Muhammad Daniyal', '1'),
+(4, 'asus.daniyal@gmail.com', 'Niyall', '1QA1qa'),
+(5, 'asus.daniyal@gmail.com', 'Muhammad Daniyal', '123qweASD'),
+(6, 'asus.daniyal@gmail.com', 'Muhammad Daniyal', '1qa2WAS');
 
 --
 -- Indexes for dumped tables
@@ -95,13 +104,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `skrining`
 --
 ALTER TABLE `skrining`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
